15a366e0ed726e6c60e72805275b2721b10c3147
6alex6kash6 <6alex6kash6@gmail.com>
2025-05-12 16:44:44 +0300

    Fix review imports and keys to match new file names and slugs; add 8x8 reviews and update goto, imo, phone-com, justcall, and textfree mappings.

--- src/data/reviews.ts
+++ src/data/reviews.ts
@@ -1,5 +1,31 @@
-import {viberReviews} from "./viber";
-import {yollaReviews} from "./yolla";
+import { viberReviews } from "./viber";
+import { yollaReviews } from "./yolla";
+import { zadarmaReviews } from "./zadarma";
+import { webexReviews } from "./webex";
+import { voipbusterReviews } from "./voipbuster";
+import { truphoneReviews } from "./truphone";
+import { sonetelReviews } from "./sonetel";
+import { ringcentralReviews } from "./ringcentral";
+import { rebtelReviews } from "./rebtel";
+import { pinger_incReviews } from "./pinger_inc";
+import { phone_comReviews } from "./phone-com";
+import { openphoneReviews } from "./openphone";
+import { oomaReviews } from "./ooma";
+import { nextivaReviews } from "./nextiva";
+import { mightycallReviews } from "./mightycall";
+import { magicjackReviews } from "./magicjack";
+import { lineReviews } from "./line";
+import { justcall_ioReviews } from "./justcall_io";
+import { imo_messengerReviews } from "./imo";
+import { gotoReviews } from "./goto-connect";
+import { googleReviews } from "./google";
+import { frejunReviews } from "./frejun";
+import { dingtoneReviews } from "./dingtone";
+import { chantyReviews } from "./chanty";
+import { brosixReviews } from "./brosix";
+import { aircallReviews } from "./aircall";
+import { sipgateReviews } from "./sipgate_com_sipgate_brings_voip_to_your_iphone";
+import { eightXEightReviews } from "./8x8";
 
 export interface Review {
   id: number;
@@ -14,8 +40,34 @@ export interface Review {
 }
 
 export const allReviews: Record<string, Review[]> = {
-  "viber": viberReviews,
-  "yolla": yollaReviews,
+  viber: viberReviews,
+  yolla: yollaReviews,
+  zadarma: zadarmaReviews,
+  webex: webexReviews,
+  voipbuster: voipbusterReviews,
+  truphone: truphoneReviews,
+  sonetel: sonetelReviews,
+  ringcentral: ringcentralReviews,
+  rebtel: rebtelReviews,
+  textfree: pinger_incReviews,
+  "phone-com": phone_comReviews,
+  openphone: openphoneReviews,
+  ooma: oomaReviews,
+  nextiva: nextivaReviews,
+  mightycall: mightycallReviews,
+  magicjack: magicjackReviews,
+  line: lineReviews,
+  justcall: justcall_ioReviews,
+  imo: imo_messengerReviews,
+  "goto-connect": gotoReviews,
+  google: googleReviews,
+  frejun: frejunReviews,
+  dingtone: dingtoneReviews,
+  chanty: chantyReviews,
+  brosix: brosixReviews,
+  aircall: aircallReviews,
+  sipgate: sipgateReviews,
+  "8x8": eightXEightReviews,
 };
 
 export const getReviewsBySlug = (slug: string): Review[] => {
@@ -25,7 +77,7 @@ export const getReviewsBySlug = (slug: string): Review[] => {
 export const getAverageRating = (slug: string): number => {
   const reviews = getReviewsBySlug(slug);
   if (reviews.length === 0) return 0;
-  
+
   const sum = reviews.reduce((total, review) => total + review.rating, 0);
   return parseFloat((sum / reviews.length).toFixed(1));
 };
