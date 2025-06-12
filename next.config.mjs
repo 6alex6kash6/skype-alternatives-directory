85632277a54b6b304814856cb720a483d417ca9c
6alex6kash6 <6alex6kash6@gmail.com>
2025-06-12 12:26:35 +0300

    add new blog posts

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,31 @@
-import createMDX from '@next/mdx'
+import createMDX from "@next/mdx";
 
 const withMDX = createMDX({
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
   },
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.seobotai.com",
+        port: "",
+        pathname: "/**",
       },
     ],
     // domains: ["images.unsplash.com"],
   },
-}
+};
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
